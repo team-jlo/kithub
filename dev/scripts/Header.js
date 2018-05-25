f73649152cd9f39ae652bb138f8c585b2b6c57d0
Jackie Lealess <jlealess@gmail.com>
2018-05-25 14:47:27 -0400

    Adding Header.js

---
+++ dev/scripts/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = () => {
+    <header>
+        <h1>
+            <span>Kit</span>Hub
+        </h1>
+        <nav className="main-nav">
+            Nav will go here
+        </nav>
+    </header>
+}
+
+export default Header;

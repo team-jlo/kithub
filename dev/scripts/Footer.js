2c667e5d21dbfdd4344c12978e4bf915d2aed049
Laura-Louise Tobin <lauralouisetobin@gmail.com>
2018-05-29 16:41:01 -0400

    Possible merge conflict in footer

--- dev/scripts/Footer.js
+++ dev/scripts/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
 		return(
-            <footer className="footer clearfix">
+            <footer>
 
             <p>Kithub was made by Jackie Lealess, Orry Mevorach, and Laura-Louise Tobin</p>
             <p>KitHub runs on  the <a href="http://makeup-api.herokuapp.com"> Makeup API</a> </p>

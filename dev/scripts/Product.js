c370109b49e8ee2e0aab0efa065132478feb32df
Laura-Louise Tobin <lauralouisetobin@gmail.com>
2018-05-29 10:56:12 -0400

    adding styles

--- dev/scripts/Product.js
+++ dev/scripts/Product.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import firebase from 'firebase';
+import Colorset from './ColorSet';
 
 
 const Product = (props) => {
@@ -13,10 +14,13 @@ const Product = (props) => {
                     {props.name}
                 </span>
             </h3>
+
 						<div className="product__image--container">
-            <img src={props.image_link} alt={props.name} className="product__image" />
+            	<img src={props.image_link} alt={props.name} className="product__image" />
 						</div>	
-						
+
+						<Colorset />
+
             {props.loggedIn === true ? <button onClick={() => { props.addToWishlist(props.id, props.name, props.brand, props.image_link, props.description) }} className="button button__wishlist button__toggle">{props.button1Text}</button> : null}
             {props.loggedIn === true ? <button onClick={() => { props.addToKit(props.id, props.name, props.brand, props.image_link, props.description) }} className="button button__kit button__toggle">{props.button2Text}}</button> : null}
             

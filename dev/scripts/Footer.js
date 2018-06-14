eb1b0520fb2a59008a78a4e40fed17825476319c
Jackie Lealess <jlealess@gmail.com>
2018-06-14 13:35:37 -0400

    Updating contact info in footer: changed links to our portfolios

--- dev/scripts/Footer.js
+++ dev/scripts/Footer.js
@@ -1,11 +1,17 @@
 import React from 'react';
 
 const Footer = () => {
-		return(
-            <footer>
-											<hr/>
-                        <p>Kithub was made by <a href="https://github.com/jlealess/">Jackie Lealess</a>, <a href="https://github.com/orrymevorach">Orry Mevorach</a>, and <a href="https://github.com/lauralouiset">Laura-Louise Tobin</a> and runs on the <a href="http://makeup-api.herokuapp.com"> Makeup API</a> </p>
-            </footer>
-		)
+		return <footer>
+        <hr />
+        <p>
+          Kithub was made by <a href="https://www.jackielealess.com">
+            Jackie Lealess
+          </a>, <a href="https://github.com/orrymevorach">Orry Mevorach</a>, and <a href="http://lauraloui.se">
+            Laura-Louise Tobin
+          </a> and runs on the <a href="http://makeup-api.herokuapp.com">
+            Makeup API
+          </a>
+        </p>
+      </footer>;
     }
 export default Footer;
